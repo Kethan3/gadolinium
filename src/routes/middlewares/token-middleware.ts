fe748c2149acb74d9d95c6dd9570e2f487833f6e
Kethan3 <kethangowda003@gmail.com>
2025-03-26 08:53:30 +0530

    feat: add user controller with getMe and getAllUsers functions

--- src/routes/middlewares/token-middleware.ts
+++ src/routes/middlewares/token-middleware.ts
@@ -1,22 +1,33 @@
 import { createMiddleware } from "hono/factory";
 import { jwtsecretKey } from "../../../environment";
-import jwt from "jsonwebtoken" ;
+import jwt from "jsonwebtoken";
 
-export const tokenMiddleware = createMiddleware(async (context, next) => {
-    const token = context.req.header("token");
-    if (!token) {
-      return context.json(
-        {
-          message: "missing Token",
-        },
-        401
-      );
+export const tokenMiddleware = createMiddleware<{
+  Variables: {
+    userId: string;
+  };
+}>(async (context, next) => {
+  const token = context.req.header("token");
+  if (!token) {
+    return context.json(
+      {
+        message: "missing Token",
+      },
+      401
+    );
+  }
+
+  try {
+    const payload = jwt.verify(token, jwtsecretKey) as jwt.JwtPayload;
+
+    const userId = payload.sub;
+
+    if (userId) {
+      context.set("userId", userId);
     }
 
-    try {
-      const verified = jwt.verify(token, jwtsecretKey);
-      await next();
-    } catch (e) {
-      return context.json({ message: "Unauthorized or missing token" }, 401);
-    }
-  },)
+    await next();
+  } catch (e) {
+    return context.json({ message: "Unauthorized token" }, 401);
+  }
+});
--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,15 +1,56 @@
 import { Hono } from "hono";
 import { prismaClient } from "../extras/prisma";
 import { tokenMiddleware } from "./middlewares/token-middleware";
+import { getAllUsers, getMe } from "../controllers/users/users-controller";
+import { GetMeError } from "../controllers/users/users-types";
 
 export const usersRoutes = new Hono();
 
-usersRoutes.get(
-  "",
-  tokenMiddleware,
-  async (context) => {
-    const users = await prismaClient.user.findMany();
+usersRoutes.get("/me", tokenMiddleware, async (context) => {
+  const userId = context.get("userId");
+  try {
+    const user = await getMe({
+      userId,
+    });
 
-    return context.json(users, 200);
+    return context.json(
+      {
+        data: user,
+      },
+      200
+    );
+  } catch (e) {
+    if (e === GetMeError.BAD_REQUEST) {
+      return context.json(
+        {
+          error: "User not found",
+        },
+        400
+      );
+    }
+
+    return context.json(
+      {
+        message: "Internal Server Error",
+      },
+      500
+    );
   }
-);
+});
+
+usersRoutes.get("", tokenMiddleware, async (context) => {
+  try {
+    const users = await getAllUsers();
+
+    context.json(
+      {
+        data: users,
+      },
+      200
+    );
+  } catch (e) {
+    context.json({
+      message: "Internal Server Error",
+    });
+  }
+});
